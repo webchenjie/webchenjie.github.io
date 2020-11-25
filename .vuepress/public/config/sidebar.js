0dfad6b9f33848e6ce92da6e3c2c360e3d610744
webchenjie <785801610@qq.com>
2020-11-25 23:18:18 +0800

    blog

--- .vuepress/public/config/sidebar.js
+++ .vuepress/public/config/sidebar.js
@@ -134,6 +134,10 @@ module.exports = {
           title: '进阶知识',
           path: '/docs/vue/进阶知识.md',
         },
+        {
+          title: '高级知识',
+          path: '/docs/vue/高级知识.md',
+        },
       ],
     },
     {
