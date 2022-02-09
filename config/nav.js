335a36ec538d8973a8e6fb96b2e6f5fdf9a58284
chenjie <785801610@qq.com>
2022-02-09 23:31:42 +0800

    vite

--- config/nav.js
+++ config/nav.js
@@ -1,297 +1,301 @@
 module.exports = [
-	{
-		text: '主页',
-		link: '/',
-		icon: 'reco-home'
-	},
-	{
-		text: '文档',
-		icon: 'reco-api',
-		items: [
-			{
-				text: 'HTML',
-				link: '/docs/HTML/基础知识.md'
-			},
-			{
-				text: 'CSS',
-				link: '/docs/CSS/基础知识.md'
-			},
-			{
-				text: 'JavaScript',
-				link: '/docs/JavaScript/String.md'
-			},
-			{
-				text: 'TypeScript',
-				link: '/docs/TypeScript/基础知识.md'
-			},
-			{
-				text: 'jQuery',
-				link: '/docs/jQuery/基础知识.md'
-			},
-			{
-				text: 'Vue',
-				link: '/docs/Vue/基础知识.md'
-			},
-			{
-				text: 'React',
-				link: '/docs/React/基础知识.md'
-			},
-			{
-				text: 'Node',
-				link: '/docs/Node/基础知识.md'
-			},
-			{
-				text: 'Webpack',
-				link: '/docs/Webpack/基础知识.md'
-			},
-			{
-				text: 'vue-element-admin',
-				link: '/docs/vue-element-admin/基础知识.md'
-			},
-			{
-				text: 'uni-app',
-				link: '/docs/uni-app/基础知识.md'
-			},
-			{
-				text: 'Git',
-				link: '/docs/Git/基础知识.md'
-			},
-			{
-				text: 'SQL',
-				link: '/docs/SQL/基础知识.md'
-			},
-			{
-				text: 'PM2',
-				link: '/docs/PM2/基础知识.md'
-			},
-			{
-				text: 'HTTP',
-				link: '/docs/HTTP/基础知识.md'
-			},
-			{
-				text: 'Nginx',
-				link: '/docs/Nginx/基础知识.md'
-			},
-			{
-				text: 'Docker',
-				link: '/docs/Docker/基础知识.md'
-			},
-			{
-				text: 'Linux',
-				link: '/docs/Linux/基础知识.md'
-			},
-			{
-				text: '微信小程序',
-				link: '/docs/微信小程序/基础知识.md'
-			},
-			{
-				text: '性能优化',
-				link: '/docs/性能优化/基础知识.md'
-			},
-			{
-				text: '数据可视化',
-				link: '/docs/数据可视化/基础知识.md'
-			},
-			{
-				text: '自动化测试',
-				link: '/docs/自动化测试/基础知识.md'
-			},
-			{
-				text: '数据结构与算法',
-				link: '/docs/数据结构与算法/基础知识.md'
-			},
-			{
-				text: '计算机原理',
-				link: '/docs/计算机原理/基础知识.md'
-			}
-		]
-	},
-	{
-		text: '时间线',
-		link: '/timeline/',
-		icon: 'reco-date'
-	},
-	{
-		text: '学习链接',
-		icon: 'reco-document',
-		items: [
-			{
-				text: '码云',
-				link: 'https://gitee.com'
-			},
-			{
-				text: '阿里云',
-				link: 'https://account.aliyun.com/login/login.html'
-			},
-			{
-				text: '七牛云',
-				link: 'https://developer.qiniu.com'
-			},
-			{
-				text: 'NATAPP',
-				link: 'https://natapp.cn'
-			},
-			{
-				text: 'ApplicationLoader',
-				link: 'http://www.applicationloader.net'
-			},
-			{
-				text: 'BootCDN',
-				link: 'https://www.bootcdn.cn'
-			},
-			{
-				text: 'iconfont',
-				link: 'https://www.iconfont.cn'
-			},
-			{
-				text: 'TinyPNG',
-				link: 'https://tinypng.com'
-			},
-			{
-				text: 'AnimateCss',
-				link: 'https://animate.style/'
-			},
-			{
-				text: 'VelocityJs',
-				link: 'http://shouce.jb51.net/velocity/index.html'
-			},
-			{
-				text: 'SortableJs',
-				link: 'http://www.sortablejs.com/index.html'
-			},
-			{
-				text: 'MomentJs',
-				link: 'http://momentjs.cn'
-			},
-			{
-				text: 'JavaScript',
-				link: 'https://developer.mozilla.org/zh-CN/docs/Web/JavaScript'
-			},
-			{
-				text: 'TypeScript',
-				link: 'https://www.tslang.cn'
-			},
-			{
-				text: 'NodeJs',
-				link: 'http://nodejs.cn'
-			},
-			{
-				text: 'npm',
-				link: 'https://www.npmjs.com'
-			},
-			{
-				text: 'Webpack',
-				link: 'https://www.webpackjs.com'
-			},
-			{
-				text: 'UmiJs',
-				link: 'https://umijs.org/zh-CN'
-			},
-			{
-				text: 'ReactJs',
-				link: 'https://react.docschina.org'
-			},
-			{
-				text: 'VueJs',
-				link: 'https://cn.vuejs.org'
-			},
-			{
-				text: 'Vue3Js',
-				link: 'https://composition-api.vuejs.org/zh'
-			},
-			{
-				text: 'NuxtJs',
-				link: 'https://www.nuxtjs.cn'
-			},
-			{
-				text: 'vuepress',
-				link: 'https://www.vuepress.cn'
-			},
-			{
-				text: 'vuepress-theme-reco',
-				link: 'https://vuepress-theme-reco.recoluan.com'
-			},
-			{
-				text: 'vue-element-admin',
-				link: 'https://panjiachen.gitee.io/vue-element-admin-site/zh'
-			},
-			{
-				text: 'D2Admin',
-				link: 'https://d2.pub/zh/doc/d2-admin'
-			},
-			{
-				text: 'AntDesign',
-				link: 'https://ant.design/docs/react/introduce-cn'
-			},
-			{
-				text: 'element-ui',
-				link: 'https://element.eleme.cn/#/zh-CN'
-			},
-			{
-				text: 'element-plus-ui',
-				link: 'https://element-plus.gitee.io/#/zh-CN/component/installation'
-			},
-			{
-				text: 'VantUi',
-				link: 'https://vant-contrib.gitee.io/vant/#/zh-CN/'
-			},
-			{
-				text: 'uViewUi',
-				link: 'https://www.uviewui.com'
-			},
-			{
-				text: 'uni-app',
-				link: 'https://uniapp.dcloud.io'
-			},
-			{
-				text: 'uniCloud',
-				link: 'https://unicloud.dcloud.net.cn/login'
-			},
-			{
-				text: 'uni-app可视化组件API',
-				link: 'https://hellouniapp.dcloud.net.cn/pcguide.html'
-			},
-			{
-				text: 'axios',
-				link: 'http://www.axios-js.com/zh-cn/docs'
-			},
-			{
-				text: 'luch-request',
-				link: 'https://www.quanzhan.co/luch-request'
-			},
-			{
-				text: 'Swiper',
-				link: 'https://www.swiper.com.cn'
-			},
-			{
-				text: 'Echarts',
-				link: 'https://echarts.apache.org/zh/index.html'
-			},
-			{
-				text: 'uCharts',
-				link: 'http://doc.ucharts.cn/1073940'
-			},
-			{
-				text: 'wangEditor',
-				link: 'http://www.wangeditor.com/doc'
-			},
-			{
-				text: '微信开发文档',
-				link: 'https://developers.weixin.qq.com/doc/'
-			},
-			{
-				text: '微信开放平台',
-				link: 'https://open.weixin.qq.com'
-			},
-			{
-				text: '微信公众平台',
-				link: 'https://mp.weixin.qq.com'
-			}
-		]
-	},
-	{
-		text: 'GitHub',
-		link: 'https://github.com/webchenjie',
-		icon: 'reco-github'
-	}
+  {
+    text: '主页',
+    link: '/',
+    icon: 'reco-home'
+  },
+  {
+    text: '文档',
+    icon: 'reco-api',
+    items: [
+      {
+        text: 'HTML',
+        link: '/docs/HTML/基础知识.md'
+      },
+      {
+        text: 'CSS',
+        link: '/docs/CSS/基础知识.md'
+      },
+      {
+        text: 'JavaScript',
+        link: '/docs/JavaScript/String.md'
+      },
+      {
+        text: 'TypeScript',
+        link: '/docs/TypeScript/基础知识.md'
+      },
+      {
+        text: 'jQuery',
+        link: '/docs/jQuery/基础知识.md'
+      },
+      {
+        text: 'Vue',
+        link: '/docs/Vue/基础知识.md'
+      },
+      {
+        text: 'React',
+        link: '/docs/React/基础知识.md'
+      },
+      {
+        text: 'Node',
+        link: '/docs/Node/基础知识.md'
+      },
+      {
+        text: 'Webpack',
+        link: '/docs/Webpack/基础知识.md'
+      },
+      {
+        text: 'Vite',
+        link: '/docs/Vite/基础知识.md'
+      },
+      {
+        text: 'vue-element-admin',
+        link: '/docs/vue-element-admin/基础知识.md'
+      },
+      {
+        text: 'uni-app',
+        link: '/docs/uni-app/基础知识.md'
+      },
+      {
+        text: 'Git',
+        link: '/docs/Git/基础知识.md'
+      },
+      {
+        text: 'SQL',
+        link: '/docs/SQL/基础知识.md'
+      },
+      {
+        text: 'PM2',
+        link: '/docs/PM2/基础知识.md'
+      },
+      {
+        text: 'HTTP',
+        link: '/docs/HTTP/基础知识.md'
+      },
+      {
+        text: 'Nginx',
+        link: '/docs/Nginx/基础知识.md'
+      },
+      {
+        text: 'Docker',
+        link: '/docs/Docker/基础知识.md'
+      },
+      {
+        text: 'Linux',
+        link: '/docs/Linux/基础知识.md'
+      },
+      {
+        text: '微信小程序',
+        link: '/docs/微信小程序/基础知识.md'
+      },
+      {
+        text: '性能优化',
+        link: '/docs/性能优化/基础知识.md'
+      },
+      {
+        text: '数据可视化',
+        link: '/docs/数据可视化/基础知识.md'
+      },
+      {
+        text: '自动化测试',
+        link: '/docs/自动化测试/基础知识.md'
+      },
+      {
+        text: '数据结构与算法',
+        link: '/docs/数据结构与算法/基础知识.md'
+      },
+      {
+        text: '计算机原理',
+        link: '/docs/计算机原理/基础知识.md'
+      }
+    ]
+  },
+  {
+    text: '时间线',
+    link: '/timeline/',
+    icon: 'reco-date'
+  },
+  {
+    text: '学习链接',
+    icon: 'reco-document',
+    items: [
+      {
+        text: '码云',
+        link: 'https://gitee.com'
+      },
+      {
+        text: '阿里云',
+        link: 'https://account.aliyun.com/login/login.html'
+      },
+      {
+        text: '七牛云',
+        link: 'https://developer.qiniu.com'
+      },
+      {
+        text: 'NATAPP',
+        link: 'https://natapp.cn'
+      },
+      {
+        text: 'ApplicationLoader',
+        link: 'http://www.applicationloader.net'
+      },
+      {
+        text: 'BootCDN',
+        link: 'https://www.bootcdn.cn'
+      },
+      {
+        text: 'iconfont',
+        link: 'https://www.iconfont.cn'
+      },
+      {
+        text: 'TinyPNG',
+        link: 'https://tinypng.com'
+      },
+      {
+        text: 'AnimateCss',
+        link: 'https://animate.style/'
+      },
+      {
+        text: 'VelocityJs',
+        link: 'http://shouce.jb51.net/velocity/index.html'
+      },
+      {
+        text: 'SortableJs',
+        link: 'http://www.sortablejs.com/index.html'
+      },
+      {
+        text: 'MomentJs',
+        link: 'http://momentjs.cn'
+      },
+      {
+        text: 'JavaScript',
+        link: 'https://developer.mozilla.org/zh-CN/docs/Web/JavaScript'
+      },
+      {
+        text: 'TypeScript',
+        link: 'https://www.tslang.cn'
+      },
+      {
+        text: 'NodeJs',
+        link: 'http://nodejs.cn'
+      },
+      {
+        text: 'npm',
+        link: 'https://www.npmjs.com'
+      },
+      {
+        text: 'Webpack',
+        link: 'https://www.webpackjs.com'
+      },
+      {
+        text: 'UmiJs',
+        link: 'https://umijs.org/zh-CN'
+      },
+      {
+        text: 'ReactJs',
+        link: 'https://react.docschina.org'
+      },
+      {
+        text: 'VueJs',
+        link: 'https://cn.vuejs.org'
+      },
+      {
+        text: 'Vue3Js',
+        link: 'https://composition-api.vuejs.org/zh'
+      },
+      {
+        text: 'NuxtJs',
+        link: 'https://www.nuxtjs.cn'
+      },
+      {
+        text: 'vuepress',
+        link: 'https://www.vuepress.cn'
+      },
+      {
+        text: 'vuepress-theme-reco',
+        link: 'https://vuepress-theme-reco.recoluan.com'
+      },
+      {
+        text: 'vue-element-admin',
+        link: 'https://panjiachen.gitee.io/vue-element-admin-site/zh'
+      },
+      {
+        text: 'D2Admin',
+        link: 'https://d2.pub/zh/doc/d2-admin'
+      },
+      {
+        text: 'AntDesign',
+        link: 'https://ant.design/docs/react/introduce-cn'
+      },
+      {
+        text: 'element-ui',
+        link: 'https://element.eleme.cn/#/zh-CN'
+      },
+      {
+        text: 'element-plus-ui',
+        link: 'https://element-plus.gitee.io/#/zh-CN/component/installation'
+      },
+      {
+        text: 'VantUi',
+        link: 'https://vant-contrib.gitee.io/vant/#/zh-CN/'
+      },
+      {
+        text: 'uViewUi',
+        link: 'https://www.uviewui.com'
+      },
+      {
+        text: 'uni-app',
+        link: 'https://uniapp.dcloud.io'
+      },
+      {
+        text: 'uniCloud',
+        link: 'https://unicloud.dcloud.net.cn/login'
+      },
+      {
+        text: 'uni-app可视化组件API',
+        link: 'https://hellouniapp.dcloud.net.cn/pcguide.html'
+      },
+      {
+        text: 'axios',
+        link: 'http://www.axios-js.com/zh-cn/docs'
+      },
+      {
+        text: 'luch-request',
+        link: 'https://www.quanzhan.co/luch-request'
+      },
+      {
+        text: 'Swiper',
+        link: 'https://www.swiper.com.cn'
+      },
+      {
+        text: 'Echarts',
+        link: 'https://echarts.apache.org/zh/index.html'
+      },
+      {
+        text: 'uCharts',
+        link: 'http://doc.ucharts.cn/1073940'
+      },
+      {
+        text: 'wangEditor',
+        link: 'http://www.wangeditor.com/doc'
+      },
+      {
+        text: '微信开发文档',
+        link: 'https://developers.weixin.qq.com/doc/'
+      },
+      {
+        text: '微信开放平台',
+        link: 'https://open.weixin.qq.com'
+      },
+      {
+        text: '微信公众平台',
+        link: 'https://mp.weixin.qq.com'
+      }
+    ]
+  },
+  {
+    text: 'GitHub',
+    link: 'https://github.com/webchenjie',
+    icon: 'reco-github'
+  }
 ]
