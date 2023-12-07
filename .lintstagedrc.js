ce4da944774a04ad02d2990aade802f0895c5a5e
chenjie <chenjie@szy.cn>
2023-12-07 17:13:57 +0800

    chore: update config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  '*': 'prettier --write --cache --ignore-unknown',
   '*.{vue,js,jsx,ts,tsx}': 'eslint --fix',
-  '*.{vue,css,less,sass,scss}': 'stylelint --fix'
+  '*.{vue,css,less,sass,scss}': 'stylelint --fix --allow-empty-input'
 }
